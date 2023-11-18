6e737b59cf1907a064ed15d9f4b9e35021d7696d
Joseph <51335923+Otterboi@users.noreply.github.com>
2023-11-18 17:14:02 -0500

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,7 +1,7 @@
  -- DDL
 CREATE TABLE Entertainment (
     entertainment_id INT AUTO_INCREMENT PRIMARY KEY,
-    type ENUM('Movie', 'Serie', 'Anime'),
+    type ENUM('Movie', 'TV Show', 'Anime'),
     title VARCHAR(255),
     description TEXT,
     rating FLOAT,
@@ -18,16 +18,30 @@ CREATE TABLE Movie (
     FOREIGN KEY (movie_id) REFERENCES Entertainment(entertainment_id)
 );
 
-CREATE TABLE Serie (
-    serie_id INT PRIMARY KEY,
+CREATE TABLE TV_Show (
+    tv_show_id INT PRIMARY KEY,
     imdb_id VARCHAR(20) UNIQUE,
     number_of_seasons INT,
-    FOREIGN KEY (serie_id) REFERENCES Entertainment(entertainment_id)
+    FOREIGN KEY (tv_show_id) REFERENCES Entertainment(entertainment_id)
 );
 
 CREATE TABLE Anime (
     anime_id INT PRIMARY KEY,
-    FOREIGN KEY (anime_id) REFERENCES Serie(serie_id)
+    FOREIGN KEY (anime_id) REFERENCES TV_Show(tv_show_id)
+);
+
+CREATE TABLE Season_TV_Show (
+    season_tv_show_id INT AUTO_INCREMENT PRIMARY KEY,
+    season_id INT,
+    title VARCHAR(255),
+    FOREIGN KEY (season_id) REFERENCES TV_Show(tv_show_id)
+);
+
+CREATE TABLE Season_Anime(
+    season_anime_id INT AUTO_INCREMENT PRIMARY KEY,
+    anime_id INT,
+    title VARCHAR(255),
+    FOREIGN KEY (anime_id) REFERENCES Anime(anime_id)
 );
 
 CREATE TABLE Person (
@@ -144,54 +158,54 @@ CREATE TABLE MovieLanguage (
     FOREIGN KEY (language_id) REFERENCES Language(language_id)
 );
 
-CREATE TABLE SerieCreator (
-    serie_id  INT,
+CREATE TABLE TVShowCreator (
+    tv_show_id  INT,
     creator_id INT,
-    PRIMARY KEY (serie_id , creator_id),
-    FOREIGN KEY (serie_id ) REFERENCES Serie(serie_id ),
+    PRIMARY KEY (tv_show_id , creator_id),
+    FOREIGN KEY (tv_show_id ) REFERENCES TV_Show(tv_show_id ),
     FOREIGN KEY (creator_id) REFERENCES Creator(creator_id)
 );
 
-CREATE TABLE SerieGenre (
-    serie_id INT,
+CREATE TABLE TVShowGenre (
+    tv_show_id INT,
     genre_id INT,
-    PRIMARY KEY (serie_id, genre_id),
-    FOREIGN KEY (serie_id) REFERENCES Serie(serie_id),
+    PRIMARY KEY (tv_show_id, genre_id),
+    FOREIGN KEY (tv_show_id) REFERENCES TV_Show(tv_show_id),
     FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
 );
 
-CREATE TABLE SerieKeyword (
-    serie_id  INT,
+CREATE TABLE TVShowKeyword (
+    tv_show_id  INT,
     keyword_id INT,
-    PRIMARY KEY (serie_id , keyword_id),
-    FOREIGN KEY (serie_id ) REFERENCES Serie(serie_id ),
+    PRIMARY KEY (tv_show_id , keyword_id),
+    FOREIGN KEY (tv_show_id ) REFERENCES TV_Show(tv_show_id ),
     FOREIGN KEY (keyword_id) REFERENCES Keyword(keyword_id)
 );
 
-CREATE TABLE SerieCountry (
-    serie_id  INT,
+CREATE TABLE TVShowCountry (
+    tv_show_id  INT,
     country_id INT,
-    PRIMARY KEY (serie_id , country_id),
-    FOREIGN KEY (serie_id ) REFERENCES Serie(serie_id ),
+    PRIMARY KEY (tv_show_id , country_id),
+    FOREIGN KEY (tv_show_id ) REFERENCES TV_Show(tv_show_id ),
     FOREIGN KEY (country_id) REFERENCES Country(country_id)
 );
 
-CREATE TABLE SerieLanguage (
-    serie_id INT,
+CREATE TABLE TVShowLanguage (
+    tv_show_id INT,
     language_id INT,
-    PRIMARY KEY (serie_id, language_id),
-    FOREIGN KEY (serie_id) REFERENCES Serie(serie_id),
+    PRIMARY KEY (tv_show_id, language_id),
+    FOREIGN KEY (tv_show_id) REFERENCES TV_Show(tv_show_id),
     FOREIGN KEY (language_id) REFERENCES Language(language_id)
 );
 
-CREATE TABLE AnimeActor {
+CREATE TABLE AnimeActor (
     anime_id INT,
     actor_id INT,
     character_name VARCHAR(255),
     PRIMARY KEY (anime_id , actor_id, character_name),
     FOREIGN KEY (anime_id) REFERENCES Anime(anime_id),
     FOREIGN KEY (actor_id) REFERENCES Actor(actor_id)
-}
+);
 
 CREATE TABLE AnimeDirector (
 	anime_id  INT,
